2cd80b36952ddc8c008b7a76b465189f527c8447
Pedro Amaral <pedr.amaral@gmail.com>
2015-05-02 14:54:52 +0100

    routes and controllers created

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
 	has_many :comments
 
+	# override the as_json method so that all json 
+	# representations of our posts will include the comments.
 	def as_json(options = {})
 	  super(options.merge(include: :comments))
 	end
